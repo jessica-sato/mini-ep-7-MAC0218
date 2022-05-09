7f5677290ab3186813c761058ee4c61c9ac1fc31
jyns1703 <jyns1703@usp.br>
2022-05-09 18:35:01 -0300

    Add functions defineCriterio and fechaBoletim to AnalisadorDeAprovacao

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,17 @@
 package domain
 
+import criterios.CriterioDeAprovacao
+import Boletim
+
 class AnalisadorDeAprovacao {
-
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
-
+    private lateinit var criterio: CriterioDeAprovacao
+    
+    @BeforeEach
+    fun defineCriterio(criterioDeAprovacao : CriterioDeAprovacao){
+        criterio = criterioDeAprovacao
+    }
+    
+    fun fechaBoletim(boletim: Boletim) : BoletimFechado{
+        return BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs, criterio.mediaFinal(boletim), criterio.estaAprovado(boletim))
+    }
 }
